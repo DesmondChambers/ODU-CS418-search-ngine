01f80ed414eb96dac532b57a407b8aaaf9e59287
Desmond Chamber <jam2bklyn@gmail.com>
2020-10-06 13:01:52 -0400

    made updates

--- searchx/login/views.py
+++ searchx/login/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-def login(request):
-    context = {}
-    return render(request, 'login/loginpage.html', context)
+def loginpage(request):
+	if request.user.is_authenticated:
+    		return redirect('/user')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('/user')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'login/loginpage.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('/login')
--- searchx/searchx/settings.py
+++ searchx/searchx/settings.py
@@ -153,3 +153,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECTED_URL = '/user'
